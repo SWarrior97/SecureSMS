aa5a8c2ecd48e5587235eed4fa772edd1e3996e4
Carlos Eduardo SIlva Nascimento Gomes <2151424@my.ipleiria.pt>
2019-02-12 21:23:30 +0000

    enviar mensagem
    começo da encriptaçao

---
+++ app/src/main/java/Encriptacao.java
@@ -0,0 +1,114 @@
+import android.os.Build;
+import android.support.annotation.RequiresApi;
+
+import java.security.Key;
+import java.security.NoSuchAlgorithmException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+import javax.crypto.Cipher;
+import javax.crypto.NoSuchPaddingException;
+import javax.crypto.spec.SecretKeySpec;
+import 	java.util.Base64;
+
+
+/**
+ *
+ * @author Carlos Gomes
+ */
+public class Encriptacao {
+    private static final String ALGORITHM = "AES";
+    private String key = "Bar12345Bar12345";
+    private Cipher ecipher;
+    private Cipher dcipher;
+
+    public Encriptacao() {
+        init();
+    }
+
+
+
+    public String encrypt(String str) throws Exception {
+        // Encode the string into bytes using utf-8
+        Key key = generateKey();
+        ecipher.init(Cipher.ENCRYPT_MODE, key);
+        byte[] utf8 = str.getBytes("UTF8");
+
+        // Encrypt
+        byte[] enc = ecipher.doFinal(utf8);
+
+        // Encode bytes to base64 to get a string
+        //return new sun.misc.BASE64Encoder().encode(enc);
+        return "teste";
+    }
+
+    public String decrypt(String str) throws Exception {
+        Key key = generateKey();
+        dcipher.init(Cipher.DECRYPT_MODE, key);
+        // Decode base64 to get bytes
+        //byte[] dec = new sun.misc.BASE64Decoder().decodeBuffer(str);
+
+        byte[] dec = new Base64.Decoder(str);
+
+        byte[] utf8 = dcipher.doFinal(dec);
+
+        // Decode using utf-8
+        return new String(utf8, "UTF8");
+    }
+
+    private Key generateKey(){
+        Key aesKey = new SecretKeySpec(key.getBytes(), ALGORITHM);
+        return aesKey;
+    }
+
+    private void init(){
+        try {
+            ecipher = Cipher.getInstance(ALGORITHM);
+            dcipher = Cipher.getInstance(ALGORITHM);
+        } catch (NoSuchAlgorithmException ex) {
+            Logger.getLogger(Encriptacao.class.getName()).log(Level.SEVERE, null, ex);
+        } catch (NoSuchPaddingException ex) {
+            Logger.getLogger(Encriptacao.class.getName()).log(Level.SEVERE, null, ex);
+        }
+
+    }
+
+    public static String getALGORITHM() {
+        return ALGORITHM;
+    }
+
+    public String encryptWithKey(String str, String key)throws Exception {
+        // Encode the string into bytes using utf-8
+        Key chave = generateKey(key);
+        ecipher.init(Cipher.ENCRYPT_MODE, chave);
+        byte[] utf8 = str.getBytes("UTF8");
+
+        // Encrypt
+        byte[] enc = ecipher.doFinal(utf8);
+
+        // Encode bytes to base64 to get a string
+        return new sun.misc.BASE64Encoder().encode(enc);
+    }
+
+    public String decryptWithKey(String str, String key) throws Exception {
+        Key chave = generateKey(key);
+        dcipher.init(Cipher.DECRYPT_MODE, chave);
+        // Decode base64 to get bytes
+        byte[] dec = new sun.misc.BASE64Decoder().decodeBuffer(str);
+
+        byte[] utf8 = dcipher.doFinal(dec);
+
+        // Decode using utf-8
+        return new String(utf8, "UTF8");
+    }
+
+    private Key generateKey(String key) {
+        Key aesKey = new SecretKeySpec(key.getBytes(), ALGORITHM);
+        return aesKey;
+    }
+
+
+
+
+
+}
--- app/src/main/java/p2/pt/ipleiria/estg/dei/securesms/MainActivity.java
+++ app/src/main/java/p2/pt/ipleiria/estg/dei/securesms/MainActivity.java
@@ -1,46 +1,80 @@
 package p2.pt.ipleiria.estg.dei.securesms;
 
+import android.Manifest;
 import android.app.PendingIntent;
 import android.content.Intent;
+import android.content.pm.PackageManager;
 import android.net.Uri;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.telephony.SmsManager;
 import android.util.Log;
 import android.view.View;
+import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
     private EditText txtPhoneNo;
     private EditText txtMessage;
+    private Button btnSendSMS;
+    private static final int PERMISSION_REQUEST_CODE = 1;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         init();
+
+        if (android.os.Build.VERSION.SDK_INT >= android.os.Build.VERSION_CODES.M) {
+            if (checkSelfPermission(Manifest.permission.SEND_SMS) == PackageManager.PERMISSION_DENIED) {
+                Log.d("permission", "permission denied to SEND_SMS - requesting it");
+                String[] permissions = {Manifest.permission.SEND_SMS};
+
+                requestPermissions(permissions, PERMISSION_REQUEST_CODE);
+            }
+        }
+
+
+            btnSendSMS.setOnClickListener(new View.OnClickListener()
+        {
+            public void onClick(View v)
+            {
+                String phoneNo = txtPhoneNo.getText().toString();
+                String message = txtMessage.getText().toString();
+                if (phoneNo.length()>0 && message.length()>0) {
+                    sendSMS(phoneNo, message);
+                }else {
+                    Toast.makeText(getBaseContext(),
+                            "Please enter both phone number and message.",
+                            Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
     }
 
     private void init() {
         txtMessage = findViewById(R.id.txtMessage);
         txtPhoneNo = findViewById(R.id.txtPhoneNo);
+        btnSendSMS = findViewById(R.id.btnSendSMS);
     }
 
     public void onClickSend(View view) {
-        String phoneNo = txtPhoneNo.getText().toString();
+        /*String phoneNo = txtPhoneNo.getText().toString();
         String message = txtMessage.getText().toString();
 
         if (phoneNo.length()>0 && message.length()>0){
             sendSMS(phoneNo, message);
         }else{
 
-        }
+        }*/
 
     }
 
     private void sendSMS(String phoneNumber, String message)
     {
-        String smsNumber = String.format("smsto: %s",
+        /*String smsNumber = String.format("smsto: %s",
                 phoneNumber);
 
         String sms = message;
@@ -54,6 +88,10 @@ public class MainActivity extends AppCompatActivity {
             startActivity(smsIntent);
         } else {
            //Log.e(TAG, "Can't resolve app for ACTION_SENDTO Intent");
-        }
+        }*/
+        PendingIntent pi = PendingIntent.getActivity(this, 0,
+                new Intent(this, MainActivity.class), 0);
+        SmsManager sms = SmsManager.getDefault();
+        sms.sendTextMessage(phoneNumber, null, message, pi, null);
     }
 }
